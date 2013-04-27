5fac9a02646fafac49fe351b8bb40fee8c561d65
jamesl1001 <jamesl1001@msn.com>
2013-04-27 14:30:19 +0100

    Base files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="author" content="Precise Pixels">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Hex Words</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<h1>Hex Words</h1>
+</body>
+</html>
